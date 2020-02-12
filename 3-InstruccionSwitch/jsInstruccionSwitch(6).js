f175c38dcee040acad5611bbf1485ed14d8e4acd
alumno <alumno@utnfra.com.ar>
2020-02-11 22:06:46 -0300

    pimer ejerc

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,7 +2,30 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
+switch (laHora) {
+    case "7":
+    case "8":
+    case "9":
+    case "10":
+    case "11":
+    alert("Es de mañana");
+    break;
+    case "12":
+    case "13":
+    case "14":
+    case "15":
+    case "16":
+    case "17":
+    case "18":
+    alert("Es de tarde");
+    case "19":
+    case "20":
+    case "21":
+    
 
+
+    default("la hora no existe");
+}
 
 
 }//FIN DE LA FUNCIÓN
